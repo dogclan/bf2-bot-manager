47efbe49e5fb87b4a19d8252f226224698311065
cetteup <me@cetteup.com>
2022-02-13 14:08:43 +0100

    feat: Only log file path when log level is set to debug

--- src/logger.ts
+++ src/logger.ts
@@ -4,7 +4,8 @@ import Config from './config';
 const logger: Logger = new Logger({
     name: 'MainLogger',
     minLevel: Config.LOG_LEVEL as TLogLevelName,
-    displayFunctionName: false
+    displayFunctionName: false,
+    displayFilePath: Config.LOG_LEVEL == 'debug' ? 'displayAll' : 'hidden'
 });
 
 export default logger;
