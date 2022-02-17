5a3b0a55aff9267021d751586fef8bbe2d5d442f
cetteup <me@cetteup.com>
2022-02-17 23:18:06 +0100

    feat: Keep bots even between teams when disabling bots

--- src/http/typing.ts
+++ src/http/typing.ts
@@ -3,3 +3,26 @@ export interface CachedRequestConfig {
     timeout?: number;
     headers?: any;
 }
+
+export type BflistServer = {
+    guid: string
+    ip: string
+    port: number
+    numPlayers: number
+    maxPlayers: number
+    // omit irrelevant attributes
+    players: BflistPlayer[]
+}
+
+export type BflistPlayer = {
+    pid: number
+    name: string
+    tag: string
+    score: number
+    kills: number
+    deaths: number
+    ping: number
+    team: number
+    teamLabel: string
+    aibot: boolean
+}
