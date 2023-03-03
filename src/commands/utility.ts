06083bb563e721571fc5f3ab804e7c8ea21c98bb
cetteup <me@cetteup.com>
2023-03-03 18:24:11 +0100

    fix: Ignore case when building autocomplete choices

--- src/commands/utility.ts
+++ src/commands/utility.ts
@@ -1,10 +1,10 @@
 import BotManager from '../BotManager';
 import Server from '../server/Server';
 
-export function buildServerOptionChoices(manager: BotManager, focusedValue?: string): { name: string, value: string }[] {
+export function buildServerOptionChoices(manager: BotManager, focusedValue: string): { name: string, value: string }[] {
     return manager.getServers()
         .map((server: Server) => server.getConfig().name)
-        .filter((choice: string) => !focusedValue || choice.startsWith(focusedValue))
+        .filter((choice: string) => choice.toLowerCase().startsWith(focusedValue.toLowerCase()))
         .map((choice: string) => ({ name: choice, value: choice }));
 }
 
