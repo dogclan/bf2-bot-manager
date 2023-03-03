0dd3fce5820d8835b8222f575532700f689d9a40
cetteup <me@cetteup.com>
2023-03-03 18:39:18 +0100

    fix: Ensure Discord limits for autocomplete choices are not breached

--- src/commands/utility.ts
+++ src/commands/utility.ts
@@ -1,10 +1,15 @@
 import BotManager from '../BotManager';
 import Server from '../server/Server';
+import Constants from '../constants';
 
 export function buildServerOptionChoices(manager: BotManager, focusedValue: string): { name: string, value: string }[] {
     return manager.getServers()
         .map((server: Server) => server.getConfig().name)
         .filter((choice: string) => choice.toLowerCase().includes(focusedValue.toLowerCase()))
+        // Avoid causing errors with long names
+        .filter((choice) => choice.length <= Constants.DISCORD_CHOICES_MAX_LENGTH)
+        // Avoid causing errors with too many options
+        .slice(0, Constants.DISCORD_CHOICES_MAX_LENGTH)
         .map((choice: string) => ({ name: choice, value: choice }));
 }
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 
 export default abstract class Constants {
     public static readonly RESOURCE_BINARIES = ['bots.exe', 'iconv.dll', 'libxml2.dll', 'zlib1.dll'];
+
+    public static readonly DISCORD_CHOICES_MAX_LENGTH = 25;
 }
